20e42a026e155d732bd05236c6b630e24b3bebee
Administrator <admin@example.com>
2021-08-18 19:18:14 +0200

    Add init.ps1

---
+++ README.md
@@ -0,0 +1,73 @@
+# MRHARNAUD - RECOVERY SCRIPT
+
+1) First run as administrator the init.ps1 script
+
+## DONE
+Init script
+
+## IN PROGRESS
+
+
+## TODO
+
+Windows Terminal
+Theme
+Icons"/ git bash
+sudo
+
+
+
+
+
+Atom
+Bitwarden
+Check point VPN
+Cisco Webex meetings
+Citrix Gateway endpoint Analysis
+Deepl
+Discord
+Docker Desktop
+Filezilla
+Git bash
+Github CLI
+Github Desktop
+GO Programming language
+Chrome
+jabra direct
+letsview
+logitech Option
+365
+teams
+microsoft to do
+vs code
+microsoft whiteboard
+firefox
+mysql workbench
+node.js
+notepad++
+openvpn
+virtualbox
+powertoys
+putty
+Python
+Microsoft remote Desktop
+Skype
+Slack
+Spotify
+teamviewer
+WSL2 + Ubuntu
+Vagrant
+vlc
+winscp
+wireguard
+wireshark
+ssh key
+wallpaper
+logitech options - settings ?
+Onedrive connect ?
+
+
+
+
+Optional :
+Arduino
---
+++ init.ps1
@@ -0,0 +1,42 @@
+﻿##! RUN AS ADMINISTRATOR !## 
+# Initial Setup - Requirement 
+
+##* Functions *##
+function TestChoco
+{ 
+    # Check OS
+    If ((Get-WMIObject win32_operatingsystem).name -like "Microsoft Windows 10 *"){
+        $OS="1"
+    }
+    # Check PS Version > 3
+    if ($PSVersionTable.PSVersion.Major -ge "3"){
+        $PSVer="1"
+    }
+    # Check .Net Version > 4.5
+    if ((Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Release -ge 378389 -eq "True"){
+        $NetVer="1"
+    }
+$ReturnValue = $OS -and $PSVer -and $NetVer
+return ($ReturnValue)
+} 
+
+
+##* Test Choco requirement and if ok install it *##
+If (TestChoco -eq "1")
+{
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+##* Install Puppet Bolt *##
+Write-Output 'Y' | choco install puppet-bolt 
+refreshenv
+Install-Module PuppetBolt -Confirm:$false -Force
+if (Get-InstalledModule -Name "PuppetBolt")
+{
+    [System.Windows.Forms.MessageBox]::Show("Puppet is installed")
+}
+
+exit
+
+
+
